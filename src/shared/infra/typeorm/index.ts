5cdc16b9ef6319c0ca5c142744c55763c620c8f3
MuriloFuza <muriloacademix@gmail.com>
2021-05-05 19:35:53 -0400

    Database config

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -2,6 +2,7 @@ import { Connection, createConnection, getConnectionOptions } from 'typeorm';
 
 export default async (): Promise<Connection> => {
   const defaultOptions = await getConnectionOptions();
+
   return createConnection(
     Object.assign(defaultOptions, {
       database:
